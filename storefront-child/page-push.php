ac8f6ddf4485fa742b1fd416a22da7b658c81f15
thangpd <5cm.1s.takaki@gmail.com>
2019-05-17 10:27:37 +0700

    oK

--- storefront-child/page-push.php
+++ storefront-child/page-push.php
@@ -1,2 +1,29 @@
-<?php    echo 'push' ?>
+<?php    echo 'push';
+$gitpath="/home/glvahost/glvthemes";
+
+chdir($gitpath);
+                                        $output = '';
+                                        $return = 1;
+//                                        $_str = exec("git checkout {$branch} && git pull", $output, $return);
+                                        $_str = exec("git checkout master && git pull", $output, $return_cp);
+
+
+if (!$return) {
+                                            $result = true;
+                                            if (empty($output)) {
+                                                $output = "Success update {$branch} of {$repo}" . $_str;
+                                            } else {
+                                                if (is_array($output)) {
+                                                    $output = implode(PHP_EOL, $output);
+                                                }
+                                                $output .= $_str;
+                                            }
+                                        } else {
+                                            $output = "Fail to update {$branch} of {$repo}";
+                                        }
+
+echo $output;
+
+
+ ?>
 
