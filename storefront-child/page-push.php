2f676388acb14c323415e9337a3a45221ce594ec
thangpd <5cm.1s.takaki@gmail.com>
2019-05-24 13:49:30 +0700

    test

--- storefront-child/page-push.php
+++ storefront-child/page-push.php
@@ -6,7 +6,7 @@ $repo="glvthemes";
 chdir($gitpath);
                                         $output = '';
                                         $return = 1;
-//                                        $_str = exec("git checkout {$branch} && git pull", $output, $return);
+//           test                             $_str = exec("git checkout {$branch} && git pull", $output, $return);
                                         $_str = exec("git checkout master && git pull origin master", $output, $return);
 
 
