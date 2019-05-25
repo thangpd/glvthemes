ab13302c650fbb9b3baa4cafe52bc20d973aaf5f
Thiên Tùng <erik11111@gmail.com>
2019-05-25 13:01:05 +0700

    Revert "Merge branch 'master' of https://github.com/thangpd/glvthemes"
    This reverts commit 815adaa4bf1f69aedcda1caf0dcbd823bec47689, reversing
    changes made to 176777c206122fe0252b8f82b61263726168ca38.

--- storefront-child/page-push-plugin.php
+++ storefront-child/page-push-plugin.php
@@ -1,13 +1,13 @@
 <?php echo 'push';
 $gitpath = "/home/glvahost/credglv";
-$branch  = "develop";
+$branch  = "master";
 
 $repo = "glvthemes";
 chdir( $gitpath );
 $output = '';
 $return = 1;
 //           test                             $_str = exec("git checkout {$branch} && git pull", $output, $return);
-$_str = exec( "git checkout ".$branch." && git pull origin ".$branch, $output, $return );
+$_str = exec( "git checkout master && git pull origin develop", $output, $return );
 
 
 print_r( $output );
