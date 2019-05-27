dedde19f783d0f8f69b089af4a21919fb870f2ff
Tai-solazu <tailm@solazu.com>
2019-05-27 20:34:08 +0700

    update css

---
+++ storefront/page-test-1.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-notify/0.2.0/css/bootstrap-notify.css">
+  <style>
+      
+  </style>
+</head>
+<body>
+
+<div class="container">
+<button class="btn btn-success" onclick="successClick()">Success</button>
+  <button class="btn btn-info" onclick="infoClick()">Information</button>
+  <button class="btn btn-warning" onclick="warningClick()">Warning</button>
+  <button class="btn btn-danger" onclick="dangerClick()">Danger</button>
+  <button class="btn btn-primary" onclick="primaryClick()">Primary</button>
+<button class="btn btn-default" onclick="defaultClick()">Default</button>
+  <button class="btn btn-link" onclick="linkClick()">Link</button>
+</div>
+<script type='text/javascript' src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script type='text/javascript' src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script type='text/javascript' src="https://cdn.jsdelivr.net/npm/bootstrap-notify@3.1.3/bootstrap-notify.min.js "></script>
+  <script type='text/javascript' src='http://wp.dev.com/wp-content/themes/glvthemes/storefront-child/assets/js/store-child.js?ver=5.2'></script>
+</body>
+</html>
