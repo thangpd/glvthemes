3944d66d2bb9073cf6ea6de7cbe7500c87d26c8a
no <leno2796@gmail.com>
2019-05-31 22:52:25 +0700

    test

--- storefront/inc/storefront-template-functions.php
+++ storefront/inc/storefront-template-functions.php
@@ -551,12 +551,14 @@ if ( ! function_exists( 'storefront_post_taxonomy' ) ) {
 
         <aside class="entry-taxonomy">
 			<?php if ( $categories_list ) : ?>
-                <div class="cat-links">
-					<?php echo wp_kses_post( $categories_list ); ?>
-                </div>
+                
 			<?php endif; ?>
 
-			
+			<?php if ( $tags_list ) : ?>
+                <div class="tags-links">
+					<?php echo esc_html( _n( 'Tag:', 'Tags:', count( get_the_tags() ), 'storefront' ) ); ?><?php echo wp_kses_post( $tags_list ); ?>
+                </div>
+			<?php endif; ?>
         </aside>
 
 		<?php
