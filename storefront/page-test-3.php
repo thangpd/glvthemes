56ad0472ed336109ca58732a57b32938da60029c
Tai-solazu <tailm@solazu.com>
2019-06-02 20:53:10 +0700

    update css

--- storefront/page-test-3.php
+++ storefront/page-test-3.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
     <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicon.ico">
+    <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,700,700i,800,800i,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css" rel="stylesheet" />
     <style>
