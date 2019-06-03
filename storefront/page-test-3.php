a67dfe357d8802e220afbf0d69383caa10f8893c
Tai-solazu <tailm@solazu.com>
2019-06-03 22:17:31 +0700

    update css

--- storefront/page-test-3.php
+++ storefront/page-test-3.php
@@ -640,6 +640,10 @@ body {
         $('.section-select .button-close').on('click', function () {
           $('.js-select2').select2('close');
         });
+
+        $(".js-select2").on('select2:open', function () {
+          $('.select2-search input').prop('focus', 0);
+      });
     });
     </script>
   </body>
