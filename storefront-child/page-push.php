b7628d8348b57ca485e11ed0bef9681fddfbfff4
thangpd <5cm.1s.takaki@gmail.com>
2019-06-06 18:47:57 +0700

    update theme push

--- storefront-child/page-push.php
+++ storefront-child/page-push.php
@@ -1,19 +1,29 @@
 <?php
-$gitpath = "/home/apache/glvthemes";
-$branch  = "master";
+$gitpath     = "/var/www/glvthemes/";
+$destination = "/home/credglv/test1.goldleaf-ventures.com/wp-content/themes/";
+$branch      = "develop";
 
 $repo = "glvthemes";
 chdir( $gitpath );
+
 $output = '';
 $return = 1;
-print_r( "git checkout " . $branch . " && git pull origin " . $branch ." && git push origin ". $branch);
-//           test                             $_str = exec("git checkout {$branch} && git pull", $output, $return);
+print_r( "git checkout develop && git pull origin develop" );
+//test $_str = exec("sudo git checkout {$branch} && git pull", $output, $return);
 
-$_str = exec( "git checkout " . $branch . " && git pull origin " . $branch." && git push origin ".$branch, $output, $return );
+$_str = exec( "sudo git checkout develop && sudo git pull origin develop", $output, $return );
+
 if ( ! $return ) {
 	$result = true;
 	if ( empty( $output ) ) {
 		$output = "Success update {$branch} of {$repo}" . $_str;
+		$_str = exec( "rsync -avz ".$gitpath." ". $destination ." --exclude .git/ --exclude .gitignore", $output, $return );
+		echo '<pre>';
+		print_r( $output );
+		echo '</pre>';
+		echo '<pre>';
+		print_r( $return );
+		echo '</pre>';
 	} else {
 		if ( is_array( $output ) ) {
 			$output = implode( PHP_EOL, $output );
