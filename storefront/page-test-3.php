e5de16c412c47f3278b4412c66ab934a7e182194
Tai-solazu <tailm@solazu.com>
2019-06-09 14:08:15 +0700

    update css

--- storefront/page-test-3.php
+++ storefront/page-test-3.php
@@ -905,7 +905,8 @@ body {
         placeholder: "Select a country",
         templateResult: formatCountry,
         templateSelection: formatCountry,
-        data: isoCountries
+        data: isoCountries,
+        maximumSelectionLength: 7
       });
       $('.section-select .button-close').on('click', function () {
         $('.js-select2').select2('close');
