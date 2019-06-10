2a909496301ee247f897b5a207044120f90fda37
no <leno2796@gmail.com>
2019-06-10 13:51:18 +0700

    update css

--- storefront/page-test-3.php
+++ storefront/page-test-3.php
@@ -920,7 +920,7 @@ body {
       $('.js-select2').on('select2:open', function() {
           $('.select2-search input').prop('focus', -1);
           $("#select option[value='0']").remove();
-          $('.select2-dropdown').hide().fadeIn(500);
+          $('.select2-dropdown').fadeIn(500);
       });
 
 
