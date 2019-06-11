05a4ea17cb82079886f31aba7e13a1a484a51bb8
thanhhieu0195 <thanhhieu0195@gmail.com>
2019-06-11 23:46:31 +0700

    change logic share link

--- storefront/inc/storefront-template-functions.php
+++ storefront/inc/storefront-template-functions.php
@@ -200,8 +200,8 @@ if ( ! function_exists( 'storefront_site_branding' ) ) {
         <div class="btn-share dropdown">
             <a href="javascript:void(0)" class="js-btn-share"><i class="flaticon-network"></i></a>
              <div id="myDropdown" class="dropdown-content">
-			    <a href="javascript:showAndroidShare("<?= $share_link ?>")">Member share</a>
-			    <a href="javascript:showAndroidShare("<?= $share_link_custom ?>")">Customer share</a>
+			    <a href="javascript:showAndroidShare('<?= $share_link ?>')">Member share</a>
+			    <a href="javascript:showAndroidShare('<?= $share_link_custom ?>')">Customer share</a>
 			  </div>
         </div>
         <script>
