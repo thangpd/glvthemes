962095e5adc74bd17a10db872ad6e49785830b1f
Tai-solazu <tailm@solazu.com>
2019-06-12 23:49:41 +0700

    update css

--- storefront/page-test-3.php
+++ storefront/page-test-3.php
@@ -935,7 +935,7 @@ body {
       $('.js-select2').on('select2:open', function() {
           $('.select2-search input').prop('focus', -1);
           $("#select option[value='0']").remove();
-          $('.select2-dropdown').hide().slideDown("fast", "easeInOutQuint");
+          $('.select2-dropdown').hide().show("fade", 500);
       });
 
 
